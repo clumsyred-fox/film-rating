15181b2f97ceb200c32ba36bc5d8628648a726f2
ligany <118294703+ligany@users.noreply.github.com>
2023-04-29 01:59:30 +0300

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,3 +9,11 @@ router_v1.register('titles', TitleGETViewSet, basename='titles')
 router_v1.register(
     r'^titles/(?P<title_id>\d+)/reviews', TitleGETViewSet, basename='titles'
 )
+v1_patterns = [
+    path('', include(router_v1.urls)),
+]
+urlpatterns = [
+    path('v1/', include(v1_patterns)),
+    path('auth/signup/', SignUpAPI.as_view(), name='signup'),
+    path('auth/token/', GetTokenAPI.as_view(), name='token'),
+]
