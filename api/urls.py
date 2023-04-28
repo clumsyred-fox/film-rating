a64bd4e7ef3724377a008e3d6b3be0db04aaa596
ligany <118294703+ligany@users.noreply.github.com>
2023-04-29 02:01:52 +0300

    Delete api directory

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-
-
-urlpatterns = [
-    path('auth/signup/', SignUpAPI.as_view(), name='signup'),
-    path('auth/token/', GetTokenAPI.as_view(), name='token'),
-]
