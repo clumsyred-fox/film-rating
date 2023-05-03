c1ef1a56f87aecc231005552c6be0b5ec10e628f
Андрей Пугач <andrewpugach17@gmail.com>
2023-05-03 16:24:21 +0300

    Views add

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+from api.views import SignUpView, AuthTokenView
+
+router = DefaultRouter()
 
 urlpatterns = [
-    path('auth/signup/', SignUpAPI.as_view(), name='signup'),
-    path('auth/token/', GetTokenAPI.as_view(), name='token'),
+    path('v1/', include(router.urls)),
+    path('auth/signup/', SignUpView.as_view(), name='signup'),
+    path('auth/token/', AuthTokenView.as_view(), name='token'),
 ]
---
+++ api/views.py
@@ -0,0 +1,62 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from api.serializers import ObtainTokenSerializer, RegistrationSerializer
+from django.core.mail import send_mail
+from django.contrib.auth.tokens import default_token_generator
+from api_yamdb.settings import FROM_EMAIL
+
+
+class AuthTokenView(APIView):
+    def post(self, request):
+        serializer = ObtainTokenSerializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.data['username']
+            confirmation_code = serializer.data['confirmation_code']
+            user = get_object_or_404(User, username=username)
+            if confirmation_code != user.confirmation_code:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            token = RefreshToken.for_user(user)
+            return Response(
+                {'token': str(token.access_token)},
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data.get('email')
+            username = serializer.validated_data.get('username')
+            confirmation_code = default_token_generator.make_token(username)
+            User.objects.create_user(
+                username=username,
+                email=email,
+                confirmation_code=confirmation_code
+            )
+            send_mail(
+                'Подтверждение регистрации на YAMDB',
+                f'Код подтверждения: {confirmation_code}',
+                from_email=FROM_EMAIL,
+                recipient_list=(serializer.data['email'],),
+                fail_silently=False,
+            )
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
